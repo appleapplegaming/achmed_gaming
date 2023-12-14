853740304f6cf5253c6245631488e35692401c18
appleapplegaming <93396573+appleapplegaming@users.noreply.github.com>
2023-12-15 00:08:19 +0100

    add jzegu command

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import discord
 import random
 import topic
 from discord.ext import commands
-
+sender = None
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -19,6 +19,18 @@ async def on_ready():
 async def topic(ctx):
     embed=discord.Embed(title=f"{random.choice(questions)}", description= "Categorie: jzegu", color = discord.Color.pink())
     await ctx.send(embed=embed)
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
+    
+
+
+@bot.command()
+async def jzegu(ctx):
+    msg = await ctx.fetch_message(ctx.message.reference.message_id)
+    await ctx.message.delete()
+    await ctx.send(f"je ziet er goed uit {msg.author.mention}")
+
 
 with open("bottoken.txt", "r") as file: TOKEN = file.read()
 bot.run(TOKEN)
