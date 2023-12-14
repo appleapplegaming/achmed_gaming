b5f5dfd8213c4dff278627c67b0ab359adb3bb43
appleapplegaming <93396573+appleapplegaming@users.noreply.github.com>
2023-12-14 23:02:45 +0100

    initial topic command

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import discord
+import random
+import topic
 from discord.ext import commands
 
 intents = discord.Intents.default()
@@ -6,6 +8,7 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='?', intents=intents)
 
+questions = topic.questions
 
 
 @bot.event
@@ -13,8 +16,9 @@ async def on_ready():
     print(f"we're live as {bot.user}")
 
 @bot.command()
-async def foo(ctx, arg):
-    await ctx.send(arg)
+async def topic(ctx):
+    embed=discord.Embed(title=f"{random.choice(questions)}", description= "Categorie: jzegu", color = discord.Color.pink())
+    await ctx.send(embed=embed)
 
 with open("bottoken.txt", "r") as file: TOKEN = file.read()
 bot.run(TOKEN)
