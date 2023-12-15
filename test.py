48e0285f63bd2da2ce06e9130763cf2f1a675f5a
appleapplegaming <93396573+appleapplegaming@users.noreply.github.com>
2023-12-15 23:16:33 +0100

    Add various commands(inshallah, mashallah, yay)

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ from discord.ext import commands
 sender = None
 intents = discord.Intents.default()
 intents.message_content = True
-
 bot = commands.Bot(command_prefix='?', intents=intents)
 
 questions = topic.questions
@@ -15,22 +14,37 @@ questions = topic.questions
 async def on_ready():
     print(f"we're live as {bot.user}")
 
+
+
 @bot.command()
 async def topic(ctx):
     embed=discord.Embed(title=f"{random.choice(questions)}", description= "Categorie: jzegu", color = discord.Color.pink())
-    await ctx.send(embed=embed)
-@bot.event
-async def on_message(message):
-    await bot.process_commands(message)
+    await ctx.reply(embed=embed)
+
+# @bot.event
+# async def on_message(message):
+#     if message.author == bot.user:
+#            return 
+#     await bot.process_commands(message)
     
 
-
+@bot.command()
+async def yay(ctx):
+      await ctx.reply(file=discord.File(r'yay.ogg'))
 @bot.command()
 async def jzegu(ctx):
     msg = await ctx.fetch_message(ctx.message.reference.message_id)
     await ctx.message.delete()
-    await ctx.send(f"je ziet er goed uit {msg.author.mention}")
+    await msg.reply(f"je ziet er goed uit")
 
+@bot.command()
+async def mashallah(ctx):
+        embed=discord.Embed(title=f"google.com wat betekent mashallah", description="Mashallah (Arabisch: ما شاء الله), ook geschreven als Maşallah of Ma Shaa Allah, is een Arabische term en betekent 'wat God heeft gewild'. Masallah wordt vaak gebruikt om waardering, vreugde, lof of dankbaarheid uit te drukken voor een bepaalde gebeurtenis of een bepaalde persoon.", color=discord.Color.green())
+        await ctx.reply(embed=embed)
 
+@bot.command()
+async def inshallah(ctx):
+        embed=discord.Embed(title=f"google.com wat betekent inshallah", description="Hoewel een gelovige in hart en nieren inderdaad “Inshallah” gebruikt om te zeggen “ja, met de wil van God,” wordt de uitdrukking ook veelvuldig gebruikt als synoniem voor “misschien,” “we zien nog wel,” “hopelijk” of “nooit van m'n leven,” aan de goede verstaander de keuze.", color=discord.Color.green())
+        await ctx.reply(embed=embed)
 with open("bottoken.txt", "r") as file: TOKEN = file.read()
 bot.run(TOKEN)
